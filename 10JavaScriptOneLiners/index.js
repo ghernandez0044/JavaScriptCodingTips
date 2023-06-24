f1d9ccffa85f2e1e618c1dff88df9b3822373753
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-23 17:31:40 -0700

    second javascript one liner

--- 10JavaScriptOneLiners/index.js
+++ 10JavaScriptOneLiners/index.js
@@ -4,3 +4,7 @@
 const array = ['a', 1, 2, 3, 'a', 'a']
 const uniqueArray = [... new Set(array)]
 console.log('uniqueArray: ', uniqueArray)
+
+// 2. Generate A Random String
+const randomString = Math.random().toString(36).slice(2)
+console.log('randomString: ', randomString)
