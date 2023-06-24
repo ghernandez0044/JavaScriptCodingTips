43c922fb20b0dbdca9644b4c1b725a23e71d9c9e
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-23 17:35:39 -0700

    third javascript one liner

--- 10JavaScriptOneLiners/index.js
+++ 10JavaScriptOneLiners/index.js
@@ -8,3 +8,10 @@ console.log('uniqueArray: ', uniqueArray)
 // 2. Generate A Random String
 const randomString = Math.random().toString(36).slice(2)
 console.log('randomString: ', randomString)
+
+// 3. Remove falsy Values From An Array
+const array2 = [0, 'a string', NaN, 5, true, undefined, false]
+const removeFalsyValues = (arr) => arr.filter(Boolean)
+console.log('removeFalsy: ', removeFalsyValues(array2))
+
+// 4. Flatten An Array
