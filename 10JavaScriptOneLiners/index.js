6d9ef4aea7123c5090157edf1540da09fc896529
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-23 17:29:01 -0700

    first javascript one liner

--- 10JavaScriptOneLiners/index.js
+++ 10JavaScriptOneLiners/index.js
@@ -1,1 +1,6 @@
 // 10 JavaScript One Liners
+
+// 1. Remove Duplicates From An Array
+const array = ['a', 1, 2, 3, 'a', 'a']
+const uniqueArray = [... new Set(array)]
+console.log('uniqueArray: ', uniqueArray)
